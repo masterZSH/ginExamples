ed4904e9dfb4c31d161c0d19d5628cd6f255a205
zsh <shaohua.zhou@joojtech.com>
2020-01-19 18:18:23 +0800

    gin练习

---
+++ index.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("/someJSON", func(c *gin.Context) {
+		data := map[string]interface{}{
+			"lang": "GO语言",
+			"tag":  "<br>",
+		}
+
+		// 输出 : {"lang":"GO\u8bed\u8a00","tag":"\u003cbr\u003e"}
+		c.AsciiJSON(http.StatusOK, data)
+	})
+
+	type LoginForm struct {
+		User     string `form:"user" binding:"required"`
+		Password string `form:"password" binding:"required"`
+	}
+
+	r.POST("/login", func(c *gin.Context) {
+		var form LoginForm
+		// 获取表单form
+		if c.ShouldBind(&form) == nil {
+			fmt.Print(form)
+		}
+	})
+
+	// 提供 unicode 实体
+	r.GET("/json", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"html": "<b>Hello, world!</b>",
+		})
+	})
+	
+	// 提供字面字符
+	r.GET("/purejson", func(c *gin.Context) {
+		c.PureJSON(200, gin.H{
+			"html": "<b>Hello, world!</b>",
+		})
+	})
+
+	r.Run() // 监听并在 0.0.0.0:8080 上启动服务
+}
