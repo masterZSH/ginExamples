ebc1296e83c743992792bf38fd70b14c6c3d550e
zsh <shaohua.zhou@joojtech.com>
2020-03-17 18:38:18 +0800

    viper绑定数据

--- config/config.go
+++ config/config.go
@@ -3,6 +3,20 @@ package config
 import "github.com/spf13/viper"
 import "fmt"
 
+type Config struct {
+	Env   string `json:"env"`
+	Mysql Mysql  `json:"mysql"`
+}
+
+type Mysql struct {
+	Host string `json:"host"`
+	Port string `json:"port"`
+	User string `json:"user"`
+	Pwd  string `json:"pwd"`
+}
+
+var SysConfig Config
+
 func Init() {
 	viper.SetConfigName("config") // name of config file (without extension)
 	viper.SetConfigType("json")   // REQUIRED if the config file does not have the extension in the name
@@ -11,5 +25,8 @@ func Init() {
 	if err != nil {               // Handle errors reading the config file
 		panic(fmt.Errorf("Fatal error config file: %s \n", err))
 	}
-	fmt.Print(viper.GetString("env"))
+	if err := viper.Unmarshal(&SysConfig); err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%+v", SysConfig)
 }
